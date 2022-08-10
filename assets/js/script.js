12d72de40bd7f42982ec7216b5c0207a2f4b7151
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-08-10 16:40:19 -0400

    fixing localstorage issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,29 +5,30 @@ $(document).ready(function(){
     $('.container-fluid').on('click', 'button', saveEvent);
   });
 
-  function displayCurrentTime(){
-    var timeDisplay = $('#currentDay');
-    var currentDay = moment().format('dddd, MMMM Do');
-    timeDisplay.text(currentDay);
-  }
+function displayCurrentTime(){
+  var timeDisplay = $('#currentDay');
+  var currentDay = moment().format('dddd, MMMM Do');
+  timeDisplay.text(currentDay);
+}
 
-  function setupTimeBackground(){
-    var timeBlocks = $('div.input-group-prepend').children('span.time-block');
+function setupTimeBackground(){
+  var timeBlocks = $('div.input-group-prepend').children('span.time-block');
     
-    var currentHour = moment().format('ha');
-    for(var i = 0; i < timeBlocks.length; i++){
-      var time = timeBlocks.eq(i).attr('data-time');
-      if(time.localeCompare(currentHour) === 0){
-        $('.form-control').eq(i).switchClass('future','present');
-        break;
-      }
-      else{
-        $('.form-control').eq(i).switchClass('future' ,'past');
-      }
-
+  var currentHour = moment().format('ha');
+  for(var i = 0; i < timeBlocks.length; i++){
+    var time = timeBlocks.eq(i).attr('data-time');
+    if(time.localeCompare(currentHour) === 0){
+      $('.form-control').eq(i).switchClass('future','present');
+      break;
+    }
+    else{
+      $('.form-control').eq(i).switchClass('future' ,'past');
     }
 
   }
+
+}
+
   function saveEvent(event){
     event.preventDefault();
     $(".container").toggleClass('d-block');
@@ -37,7 +38,17 @@ $(document).ready(function(){
     }, 3000);
     var index = $(event.target).attr('data-index');
     var text = $('.form-control').eq(index).val();
-    localStorage.setItem('events' + index, text);
-    var load = localStorage.getItem('events' + index);
-    $('.form-control').eq(index).val(load);
+    localStorage.setItem('events-' + index, text);
+  
+    var loadContent = function(){
+      if(localStorage.getItem('events-' + index) !== null){
+        $('.form-control').eq(index).val(localStorage.getItem('events-' + index));
+      }
+      else {
+        return;
+      }
+    };
+    // $('.form-control').eq(index).val(load);
+    return loadContent;
+
   }
