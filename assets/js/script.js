2c4422ed36f00839d267c2dc32a762aabc3da343
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-08-10 17:06:36 -0400

    fixed localstorage issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,27 @@ $(document).ready(function(){
     setInterval(setupTimeBackground, 30000);
     $('.container-fluid').on('click', 'button', saveEvent);
     var loadContent = function(){
-      if(localStorage.getItem('events-' + index) !== null){
-        $('.form-control').eq(index).val(localStorage.getItem('events-' + index));
+      var indexes = $('button');
+
+      for(var i = 0; i < indexes.length; i++){
+        var index = indexes.eq(i).attr('data-index');
+        if(localStorage.getItem('events-' + index) !== null){
+          $('.form-control').eq(index).val(localStorage.getItem('events-' + index));
+        }
       }
-      else {
-        return;
-      }
+      
     };
     loadContent();
   });
 
-
+/** This function uses moment.js to display the current day **/
 function displayCurrentTime(){
   var timeDisplay = $('#currentDay');
   var currentDay = moment().format('dddd, MMMM Do');
   timeDisplay.text(currentDay);
 }
 
+/** function uses moment.js to setup the background of each time block **/
 function setupTimeBackground(){
   var timeBlocks = $('div.input-group-prepend').children('span.time-block');
     
@@ -38,19 +42,21 @@ function setupTimeBackground(){
   }
 
 }
-  var index;
-  function saveEvent(event){
-    event.preventDefault();
-    $(".container").toggleClass('d-block');
-    var fade = setInterval(function(){
-      $(".container").toggleClass('d-block');
-      clearInterval(fade);
-    }, 3000);
-    var index = $(event.target).attr('data-index');
-    var text = $('.form-control').eq(index).val();
-    if(index === null && text === null){
-      return;
-    }
-    localStorage.setItem('events-' + index, text);
 
+/** event handler to be invoked when a button element is clicked on and
+ *  save the event to a localstorage object 
+ **/
+function saveEvent(event){
+  event.preventDefault();
+  $(".container").toggleClass('d-block');
+  var fade = setInterval(function(){
+    $(".container").toggleClass('d-block');
+    clearInterval(fade);
+  }, 3000);
+  var index = $(event.target).attr('data-index');
+  var text = $('.form-control').eq(index).val();
+  if(index === null && text === null){
+    return;
   }
+  localStorage.setItem('events-' + index, text);
+}
